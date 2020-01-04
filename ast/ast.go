0dd29077b7664c3bca07a48fd1135f13093d17f1
yusei-wy <yusei.kasa@gmail.com>
2020-01-04 17:45:33 +0900

    Fix Identifier が Expression の interface になっていなかった

--- ast/ast.go
+++ ast/ast.go
@@ -94,7 +94,7 @@ type Identifier struct {
 	Value string
 }
 
-func (i *Identifier) expressionStatement() {}
+func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
 func (i *Identifier) String() string       { return i.Value }
 
