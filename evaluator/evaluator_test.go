e819c7ac59ff74ad7c26cafad49e163e0715fef1
yusei-wy <yusei.kasa@gmail.com>
2020-01-19 12:48:14 +0900

    Add 条件分岐

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -87,6 +87,31 @@ func Test_BangOperator(t *testing.T) {
 	}
 }
 
+func Test_IfElseExpressions(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected interface{}
+	}{
+		{"if (true) { 10 }", 10},
+		{"if (false) { 10 }", nil},
+		{"if (1) { 10 }", 10},
+		{"if (1 < 2) { 10 }", 10},
+		{"if (1 > 2) { 10 }", nil},
+		{"if (1 < 2) { 10 } else { 20 }", 10},
+		{"if (1 > 2) { 10 } else { 20 }", 20},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		integer, ok := tt.expected.(int)
+		if ok {
+			testIntegerObject(t, evaluated, int64(integer))
+		} else {
+			testNullObject(t, evaluated)
+		}
+	}
+}
+
 func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
@@ -121,6 +146,15 @@ func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
 	if result.Value != expected {
 		t.Fatalf("object has wrong value. got=%t, want=%t",
 			result.Value, expected)
+		return false
+	}
+
+	return true
+}
+
+func testNullObject(t *testing.T, obj object.Object) bool {
+	if obj != NULL {
+		t.Fatalf("object is not Null. got=%T (%+v)", obj, obj)
 		return false
 	}
 
