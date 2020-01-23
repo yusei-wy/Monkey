d7327cfab831f8d82e707df2e334fcc31f2947cc
yusei-wy <yusei.kasa@gmail.com>
2020-01-23 21:34:25 +0900

    Add let statement のテスト

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -8,6 +8,51 @@ import (
 	"monkey/parser"
 )
 
+func Test_LetStatements(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"let a = 5; a;", 5},
+		{"let a = 5 * 5; a;", 25},
+		{"let a = 5; let b = a;", 5},
+		{"let a = 5; let b = a; let c = a + b + 5; c", 25},
+	}
+
+	for _, tt := range tests {
+		testIntegerObject(t, testEval(tt.input), tt.expected)
+	}
+}
+
+func Test_ReturnStatements(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"return 10;", 10},
+		{"return 10; 9;", 10},
+		{"return 2 * 5; 9;", 10},
+		{"9; return 2 * 5; 9", 10},
+		{
+			`
+if (10 > 1) {
+	if (10 > 1) {
+		return 10;
+	}
+
+	return 1;
+}
+`,
+			10,
+		},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
+	}
+}
+
 func Test_EvalIntegerExpression(t *testing.T) {
 	tests := []struct {
 		input    string
@@ -109,35 +154,6 @@ func Test_IfElseExpressions(t *testing.T) {
 		} else {
 			testNullObject(t, evaluated)
 		}
-	}
-}
-
-func Test_ReturnStatements(t *testing.T) {
-	tests := []struct {
-		input    string
-		expected int64
-	}{
-		{"return 10;", 10},
-		{"return 10; 9;", 10},
-		{"return 2 * 5; 9;", 10},
-		{"9; return 2 * 5; 9", 10},
-		{
-			`
-if (10 > 1) {
-	if (10 > 1) {
-		return 10;
-	}
-
-	return 1;
-}
-`,
-			10,
-		},
-	}
-
-	for _, tt := range tests {
-		evaluated := testEval(tt.input)
-		testIntegerObject(t, evaluated, tt.expected)
 	}
 }
 
