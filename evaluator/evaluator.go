e372701715d6003d32dfa002f6bf2457f41e309c
yusei-wy <yusei.kasa@gmail.com>
2020-02-08 19:45:21 +0900

    Add ハッシュリテラルの評価

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -97,6 +97,8 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 			return index
 		}
 		return evalIndexExpression(left, index)
+	case *ast.HashLiteral:
+		return evalHashLiteral(node, env)
 	}
 
 	return nil
@@ -283,6 +285,32 @@ func evalArrayIndexExpression(left, index object.Object) object.Object {
 	}
 
 	return arrObject.Elements[idx]
+}
+
+func evalHashLiteral(node *ast.HashLiteral, env *object.Environment) object.Object {
+	pairs := make(map[object.HashKey]object.HashPair)
+
+	for keyNode, valueNode := range node.Pairs {
+		key := Eval(keyNode, env)
+		if isError(key) {
+			return key
+		}
+
+		hashKey, ok := key.(object.Hashable)
+		if !ok {
+			return newError("unusable as hash key: %s", key.Type())
+		}
+
+		value := Eval(valueNode, env)
+		if isError(value) {
+			return value
+		}
+
+		hashed := hashKey.HashKey()
+		pairs[hashed] = object.HashPair{Key: key, Value: value}
+	}
+
+	return &object.Hash{Pairs: pairs}
 }
 
 func isTruthy(obj object.Object) bool {
