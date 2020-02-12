59b23a429346482bad6f119e641057ea42fc541c
yusei-wy <yusei.kasa@gmail.com>
2020-02-12 20:31:17 +0900

    Add modify をハッシュリテラルに対応

--- ast/modify_test.go
+++ ast/modify_test.go
@@ -127,4 +127,24 @@ func Test_Modify(t *testing.T) {
 			t.Errorf("not equal. got=%#v, want=%#v", modified, tt.expected)
 		}
 	}
+
+	hashLiteral := &HashLiteral{
+		Pairs: map[Expression]Expression{
+			one(): one(),
+			two(): two(),
+		},
+	}
+
+	Modify(hashLiteral, turnOneIntoTwo)
+
+	for key, val := range hashLiteral.Pairs {
+		key, _ := key.(*IntegerLiteral)
+		if key.Value != 2 {
+			t.Errorf("value is not %d. got=%d", 2, key.Value)
+		}
+		val, _ := val.(*IntegerLiteral)
+		if val.Value != 2 {
+			t.Errorf("value is not %d. got=%d", 2, val.Value)
+		}
+	}
 }
